5e8a955198dd574f62895e449032ed616f680301
Xiaodu <dujiulun2006@126.com>
2015-06-05 01:00:59 +0800

    functioning code

--- MWExtSpecialRealName/SpecialRealName.php
+++ MWExtSpecialRealName/SpecialRealName.php
@@ -4,6 +4,13 @@ class SpecialRealName extends UnlistedSpecialPage {
 		parent::__construct( 'RealName' );
 	}
 
+	protected function _getUserPage( $userRow, $userPagePath ) {
+		$title = Title::makeTitle(NS_USER, $userRow['user_name']);
+		if ($userPagePath)
+			$title = $title->getSubpage($userPagePath);
+		return $title;
+	}
+
 	function execute( $subPage ) {
 		$request = $this->getRequest();
 		$output  = $this->getOutput();
@@ -21,12 +28,33 @@ class SpecialRealName extends UnlistedSpecialPage {
 
 		if ($realName === '') {
 			// No real name provided, error
+			$output->showErrorPage('error', 'badarticleerror');
 		} else {
 			// Look up in database
+			$dbr = wfGetDB( DB_SLAVE );
+			$res = $dbr->select( 'user',
+					array('user_id', 'user_name', 'user_real_name'),
+					array('user_real_name' => $realName),
+					__METHOD__,
+					array('ORDER BY' => 'user_id ASC') );
+			$resCount = $res->numRows();
+			if ($resCount == 0) {
+				$output->showErrorPage('error', 'listusers-noresult');
+			} elseif ($resCount == 1) {
+				// Found one user
+				$row = $res->fetchRow();
+				$title = $this->_getUserPage($row, $userPagePath);
+				$url = $title->getFullURL();
+				$output->redirect($url);
+			} else {
+				// Found multiple users
+				$output->setPageTitle($this->getLocalName());
+				for ($i = 0; $i != $resCount; $i ++) {
+					$row = $res->fetchRow();
+					$title = $this->_getUserPage($row, $userPagePath);
+					$output->addWikiText("* [[" . $title->getFullText() . "]]\n");
+				}
+			}
 		}
-
-		// Test:
-		$testWikiText = '* RealName: ' . $realName . '\n* UserPagePath: ' . $userPagePath;
-		$output->addWikiText($testWikiText);
 	}
 }
